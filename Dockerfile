7b4dcf791ec775d87a74c7cacf7a3e79191cf1fa
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2026-01-02 12:17:03 +0100

    it's working

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,13 @@ VOLUME /libopus
 VOLUME /build
 
 RUN apt-get update && \
-    apt-get install -y python3 git xz-utils && \
+    apt-get install -y python3 git xz-utils curl autoconf automake libtool gcc make && \
     git clone https://github.com/emscripten-core/emsdk.git /emscrypten
 
 WORKDIR /emscrypten
 
 RUN ./emsdk install latest && \
     ./emsdk activate latest
-
-ENV EMSDK=/emscrypten
-ENV PATH="/emscrypten:${PATH}"
-
-WORKDIR /build
 
 COPY build.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 set -e
 
+# Set up Emscripten environment
+cd /emscrypten
+. /emscrypten/emsdk_env.sh
+
+# Build libopus
+cd /libopus
+./autogen.sh
+
+cd /build
 emconfigure /libopus/configure \
     --disable-asm \
-    --disable-encoder \
     --disable-doc \
     --disable-extra-programs \
     --disable-stack-protector \
     --disable-shared \
-    --enable-static
+    --enable-static \
+    --disable-rtcd \
+    --disable-dependency-tracking
 
 emmake make -j$(nproc)
+
+echo "libopus build successful"
